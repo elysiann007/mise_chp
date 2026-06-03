4ad3c3b4b80db7bcca8ab7c4bca9ef1d427cbae8
Emir (Amir) Ferdosi <131608821+elysiann007@users.noreply.github.com>
2026-06-03 21:43:52 +0300

    fix: update Google Maps URL to correct location

--- mise-frontend/src/constants/venue.ts
+++ mise-frontend/src/constants/venue.ts
@@ -5,7 +5,7 @@ export const VENUE = {
   lat: 38.4356142,
   lng: 27.1405841,
   mapUrl:
-    'https://www.google.com/maps/place/CAFE+HOOKAH+PUB+%2F+PREMIUM+HOOKAH+CLUB/@38.4356142,27.1405841,17z',
+    'https://www.google.com/maps/place/CAFE+HOOKAH+PUB+%2F+PREMIUM+HOOKAH+CLUB/@38.4359502,27.1401588,16.7z/data=!4m6!3m5!1s0x14bbd85a309d99b3:0x6e87e079c113a412!8m2!3d38.4356142!4d27.1405841!16s%2Fg%2F11cllmv5fz?entry=ttu&g_ep=EgoyMDI2MDUzMS4wIKXMDSoASAFQAw%3D%3D',
   instagram: '',
   tiktok: '',
 } as const
